662ff9211d33c78e54fa705b1a29e880facd00ae
Xotic750 <xotic750@gmail>
2015-11-21 12:56:46 +0100

    Cleanup

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * @file {@link http://xotic750.github.io/core-x/ core-x}
+ * Core bundle for project-x projects.
+ * @version 1.0.6
+ * @author Xotic750 <Xotic750@gmail.com>
+ * @copyright  Xotic750
+ * @license {@link <http://www.gnu.org/licenses/gpl-3.0.html> GPL-3.0+}
+ * @module core-x
+ */
+
+/*jslint maxlen:80, es6:false, this:true, white:true */
+
+/*jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
+  freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
+  nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
+  es3:true, esnext:false, plusplus:true, maxparams:1, maxdepth:1,
+  maxstatements:6, maxcomplexity:1 */
+
+/*global require */
+
+;(function () {
+  'use strict';
+
+  require('es5-shim');
+  require('es5-shim/es5-sham');
+  require('json3');
+  require('es6-shim');
+  module.exports.ES = require('es-abstract');
+  require('cycle-x');
+}());
