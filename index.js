802485590ad564ae4cdbdbf46c03c3b53c4e41c6
Xotic750 <xotic750@gmail>
2015-11-22 03:38:58 +0100

    Upgrade error-x

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @file {@link http://xotic750.github.io/core-x/ core-x}
  * Core bundle for project-x projects.
- * @version 1.0.21
+ * @version 1.0.22
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
